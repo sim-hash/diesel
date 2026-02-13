05bee1561d41f746d29f5e3832e46320e769caee
sim-hash <mariussiminic@protonmail.com>
2026-02-13 22:48:44 +0200

    fix compile-fail test for subselect groups

--- diesel_compile_tests/tests/fail/subselect_with_ungrouped_outer_column.rs
+++ diesel_compile_tests/tests/fail/subselect_with_ungrouped_outer_column.rs
@@ -9,26 +9,26 @@ table! {
 }
 
 table! {
-    childs {
+    children {
         id -> Integer,
         parent_id -> Integer,
         amount -> Integer,
     }
 }
 
-allow_tables_to_appear_in_same_query!(parents, childs);
+allow_tables_to_appear_in_same_query!(parents, children);
 
 fn main() {
     // Mixing an aggregate function with a subselect that references an outer
     // column should fail when the outer query has no GROUP BY clause.
     // The subquery references parents::id in its filter, which is ungrouped.
     let _ = parents::table.select((
-        diesel::dsl::count_distinct(parents::id),
-        childs::table
-            .filter(childs::parent_id.eq(parents::id))
-            .select(diesel::dsl::sum(childs::amount).assume_not_null())
+        //~^ ERROR: mixing aggregate and not aggregate expressions is not allowed in SQL
+        diesel::dsl::count(parents::id),
+        children::table
+            .filter(children::parent_id.eq(parents::id))
+            .select(diesel::dsl::sum(children::amount).assume_not_null())
             .single_value()
             .assume_not_null(),
     ));
-    //~^ ERROR
 }
